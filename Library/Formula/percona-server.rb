381354c83b2e3541255ba9659f3322b433b9bcd3
Joshua Warchol <joshua@fanzter.com>
2011-08-08 15:44:24 -0400

    Added a plist to percona-server, copying how the mysql Formula does it.

--- Library/Formula/percona-server.rb
+++ Library/Formula/percona-server.rb
@@ -10,5 +10,30 @@ class PerconaServer < Formula
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}", "--mandir=#{man}", "--infodir=#{info}"
     system "make install"
+    (prefix+'com.percona.mysqld.plist').write startup_plist
+
   end
+
+  def startup_plist; <<-EOPLIST.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>KeepAlive</key>
+      <true/>
+      <key>Label</key>
+      <string>com.percona.mysqld</string>
+      <key>Program</key>
+      <string>#{bin}/mysqld_safe</string>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>UserName</key>
+      <string>#{`whoami`.chomp}</string>
+      <key>WorkingDirectory</key>
+      <string>#{var}</string>
+    </dict>
+    </plist>
+    EOPLIST
+  end
+
 end
